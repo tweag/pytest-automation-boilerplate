3ad869011356f29322288969e022c225f51d7e3c
tauqirsarwar1 <tauqir.sarwar@pfizer.com>
2024-03-17 17:25:06 +0500

    stack behind secrets

--- api_run.sh
+++ api_run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 # Run API tests
-python -m pytest -v -s --gherkin-terminal-reporter  --html="./output/reports/" --tags=$1 --self-contained-html --reruns 1 --reruns-delay 2 --alluredir=allure-results
+python -m pytest -v -s --gherkin-terminal-reporter  --html="./output/reports/" --tags=$1 --self-contained-html --reruns 1 --reruns-delay 2 --alluredir=allure-results \
+--slack-webhook-url=$2 --slack-channel=pytest-test-automation --slack-results-url=$3 --teams-webhook-url=$4 --teams-results-url=$3
