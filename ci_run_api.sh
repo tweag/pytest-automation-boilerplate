b879b1327e01c4dbdee94d5c2ce475526e5e0f74
tauqirsarwar1 <tauqir.sarwar@pfizer.com>
2024-03-27 15:37:13 +0500

    parallel execution

--- ci_run_api.sh
+++ ci_run_api.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Run API tests
-python -m pytest -vv -s  --html="./output/reports/" --tags=$1 --self-contained-html --reruns 1 --reruns-delay 2 --alluredir=allure-results \
+python -m pytest -vv -s --html="./output/reports/" --tags=$1 --self-contained-html --reruns 1 --reruns-delay 2 --alluredir=allure-results \
 --slack-webhook-url=$2 --slack-channel=pytest-test-automation --slack-results-url=https://tweag.github.io/pytest-automation-boilerplate --teams-webhook-url=$3 \
---teams-results-url=https://tweag.github.io/pytest-automation-boilerplate -n=2
+--teams-results-url=https://tweag.github.io/pytest-automation-boilerplate -n=4
