2f256a7fb8dc7264fbbd19970c7860c662cad0b9
tauqirsarwar1 <tauqir.sarwar@pfizer.com>
2024-03-28 10:49:23 +0500

    parallel execution

--- local_run_android.sh
+++ local_run_android.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Run API tests
-python -m pytest -v -s  --disable-warnings --gherkin-terminal-reporter --driver=Appium  --html="./output/reports/" --self-contained-html  \
+python -m pytest -vv -s  --disable-warnings --gherkin-terminal-reporter --driver=Appium  --html="./output/reports/" --self-contained-html  \
 --variables="env_configs/android_mobile_docker.json"  --reruns 1 --reruns-delay 2 --tags="android_mobile_tests"
