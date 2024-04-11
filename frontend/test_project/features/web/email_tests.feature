497418557c5fc449a3c7528ac9a86e51015b052b
tauqirsarwar1 <tauqir.sarwar@pfizer.com>
2024-04-11 11:17:41 +0500

    email step

---
+++ frontend/test_project/features/web/email_tests.feature
@@ -0,0 +1,6 @@
+@nondestructive @email_tests
+Feature:  Email Test
+
+  @email @automated
+  Scenario: Email verification
+    When I get email for 'moduspytestboilerplate@gmail.com'
--- frontend/test_project/step_definitions/test_steps/test_site_steps.py
+++ frontend/test_project/step_definitions/test_steps/test_site_steps.py
@@ -6,6 +6,8 @@ from main.plugin import PROJECT_DIR
 
 scenarios(os.path.join(PROJECT_DIR, "frontend/test_project/features/web/web_tests.feature"))
 scenarios(os.path.join(PROJECT_DIR, "frontend/test_project/features/visual/visual_tests.feature"))
+scenarios(os.path.join(PROJECT_DIR, "frontend/test_project/features/web/email_tests.feature"))
+
 
 
 
